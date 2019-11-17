d90f5a97ca7ec5e7e168b3d9e5f550551bab8c84
Kelly Selden <kellyselden@gmail.com>
2019-11-17 20:40:58 +0000

    uncomment assertCodemodRan

--- test/helpers/assertions.js
+++ test/helpers/assertions.js
@@ -6,14 +6,6 @@ module.exports.assertNoUnstaged = function(status) {
   expect(status).to.not.match(/^.\w/m);
 };
 
-module.exports.assertNoStaged = function(status) {
-  expect(status).to.not.match(/^\w/m);
-};
-
 module.exports.assertCodemodRan = function(status) {
-  // codemod changed locally, no change upstream
-  expect(status).to.match(/^M {2}.*app\/controllers\/application\.js$/m);
-
-  // codemod changed locally, also changed upstream
-  expect(status).to.match(/^M {2}.*app\/router\.js$/m);
+  expect(status).to.match(/^A {2}.*/m);
 };
