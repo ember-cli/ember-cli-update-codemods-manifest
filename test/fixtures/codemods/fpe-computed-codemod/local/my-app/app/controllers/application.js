c1905d5eab1c59123966febd87ad8610cd7a3528
Rajasegar Chandran <rajasegar.c@gmail.com>
2020-01-26 19:46:30 +0530

    chore: change fixtures for fpe-computed-codemod

--- test/fixtures/codemods/fpe-computed-codemod/local/my-app/app/controllers/application.js
+++ test/fixtures/codemods/fpe-computed-codemod/local/my-app/app/controllers/application.js
@@ -3,5 +3,5 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   fullName: function() {
     return `${this.firstName} ${this.lastName}`;
-  }.computed('firstName', 'lastName')
+  }.property('firstName', 'lastName')
 });
